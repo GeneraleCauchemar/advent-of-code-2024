7bda4179a4c5538274fd18aa40edb02719f65e41
Lucille <lchoisnet@umanit.fr>
2024-12-05 22:49:42 +0100

    chore: refactoring input management
    - Creates a new service to manage input reading and splitting

--- src/ConundrumSolver/AbstractConundrumSolver.php
+++ src/ConundrumSolver/AbstractConundrumSolver.php
@@ -5,17 +5,20 @@ declare(strict_types=1);
 namespace App\ConundrumSolver;
 
 use App\Exception\InputFileNotFoundException;
+use App\Service\InputManager;
+use Symfony\Contracts\Service\Attribute\Required;
 
 abstract class AbstractConundrumSolver implements ConundrumSolverInterface
 {
-    protected const int PART_ONE = 1;
-    protected const int PART_TWO = 2;
+    public const int PART_ONE = 1;
+    public const int PART_TWO = 2;
     protected const string UNDETERMINED = 'to be determined';
 
     private bool $testMode = false;
     private array|string $input;
     private array $testInputs;
     protected ?float $executionTime = null;
+    private InputManager $inputManager;
 
     public function __construct(
         protected readonly string $year = '',
@@ -23,6 +26,19 @@ abstract class AbstractConundrumSolver implements ConundrumSolverInterface
         private readonly ?string $separator = PHP_EOL,
         private readonly bool $keepAsString = false,
     ) {
+    }
+
+    #[Required]
+    public function setInputManager(InputManager $inputManager): void
+    {
+        $this->inputManager = $inputManager;
+
+        $this->inputManager->setParameters(
+            $this->year,
+            $this->day,
+            $this->keepAsString,
+            $this->separator
+        );
     }
 
     public function supports(string $year, string $day): bool
@@ -109,71 +125,20 @@ abstract class AbstractConundrumSolver implements ConundrumSolverInterface
         $this->isTestMode() ? $this->initTestInputs() : $this->initInput();
     }
 
+    /**
+     * @throws InputFileNotFoundException
+     */
     private function initInput(): void
     {
-        $path = $this->getPathToInput();
-
-        if (!file_exists($path)) {
-            throw new InputFileNotFoundException(
-                \sprintf('Missing input file at path "%s".', $path)
-            );
-        }
-
-        $this->input = $this->getContent($path);
+        $this->input = $this->inputManager->initInput();
     }
 
+    /**
+     * @throws InputFileNotFoundException
+     */
     private function initTestInputs(): void
     {
-        $this->testInputs = [];
-        $paths = [
-            self::PART_ONE => $this->getPathToInput(true, self::PART_ONE),
-            self::PART_TWO => $this->getPathToInput(true, self::PART_TWO),
-        ];
-
-        // If there is a different test input for each part
-        foreach ($paths as $part => $path) {
-            if (!file_exists($path)) {
-                continue;
-            }
-
-            $this->testInputs[$part] = $this->getContent($path);
-        }
-
-        // If no separate test inputs were found, tries to find a common one
-        if (empty($this->testInputs)) {
-            $path = $this->getPathToInput(true);
-
-            if (file_exists($path)) {
-                $this->testInputs = array_fill_keys([self::PART_ONE, self::PART_TWO], $this->getContent($path));
-            }
-        }
-
-        if (empty($this->testInputs)) {
-            throw new InputFileNotFoundException('Missing input file(s) for testing.');
-        }
-    }
-
-    private function getContent(string $path): array|string
-    {
-        return $this->keepAsString ?
-            trim(file_get_contents($path)) :
-            array_filter(explode($this->separator, file_get_contents($path)));
-    }
-
-    private function getPathToInput(bool $forTesting = false, ?int $part = null): string
-    {
-        $format = __DIR__ . '/../../Resources/input/%s%s/%s%s.txt';
-        $values = [
-            'Year' . $this->year,
-            $forTesting ? '/test' : '',
-            $this->day,
-            null !== $part ? ('_' . $part) : '',
-        ];
-
-        return \sprintf(
-            $format,
-            ...$values
-        );
+        $this->testInputs = $this->inputManager->initTestInputs();
     }
 
     private function trackAndSolve(): array
---
+++ src/Service/InputManager.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Service;
+
+use App\ConundrumSolver\AbstractConundrumSolver;
+use App\Exception\InputFileNotFoundException;
+
+class InputManager
+{
+    private const string INPUT_PATH_FORMAT = '%s/../../Resources/input/%s%s/%s%s.txt';
+
+    private bool $keepAsString;
+    private string $separator;
+    private string $year;
+    private string $day;
+
+    public function setParameters(string $year, string $day, bool $keepAsString, string $separator): void
+    {
+        $this->year = $year;
+        $this->day = $day;
+        $this->keepAsString = $keepAsString;
+        $this->separator = $separator;
+    }
+
+    /**
+     * @throws InputFileNotFoundException
+     */
+    public function initInput(): array|string
+    {
+        $path = $this->getPathToInput();
+
+        if (!file_exists($path)) {
+            throw new InputFileNotFoundException(
+                \sprintf('Missing input file at path "%s".', $path)
+            );
+        }
+
+        return $this->getContent($path);
+    }
+
+    /**
+     * @throws InputFileNotFoundException
+     */
+    public function initTestInputs(): array
+    {
+        $inputs = [];
+        $paths = [
+            AbstractConundrumSolver::PART_ONE => $this->getPathToInput(true, AbstractConundrumSolver::PART_ONE),
+            AbstractConundrumSolver::PART_TWO => $this->getPathToInput(true, AbstractConundrumSolver::PART_TWO),
+        ];
+
+        // If there is a different test input for each part
+        foreach ($paths as $part => $path) {
+            if (!file_exists($path)) {
+                continue;
+            }
+
+            $inputs[$part] = $this->getContent($path);
+        }
+
+        // If no separate test inputs were found, tries to find a common one
+        if (empty($inputs)) {
+            $path = $this->getPathToInput(true);
+
+            if (file_exists($path)) {
+                $inputs = array_fill_keys([
+                    AbstractConundrumSolver::PART_ONE,
+                    AbstractConundrumSolver::PART_TWO,
+                ], $this->getContent($path));
+            }
+        }
+
+        if (empty($inputs)) {
+            throw new InputFileNotFoundException('Missing input file(s) for testing.');
+        }
+
+        return $inputs;
+    }
+
+    private function getPathToInput(bool $forTesting = false, ?int $part = null): string
+    {
+        $values = [
+            __DIR__,
+            'Year' . $this->year,
+            $forTesting ? '/test' : '',
+            $this->day,
+            null !== $part ? ('_' . $part) : '',
+        ];
+
+        return \sprintf(
+            self::INPUT_PATH_FORMAT,
+            ...$values
+        );
+    }
+
+    private function getContent(string $path): array|string
+    {
+        return $this->keepAsString ?
+            trim(file_get_contents($path)) :
+            array_filter(explode($this->separator, file_get_contents($path)));
+    }
+}
